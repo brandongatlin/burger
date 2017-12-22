7f04ddd58138f9d414c1e96868cec8317f371bb2
brandongatlin <brandongatlin1981@me.com>
2017-12-22 17:54:29 -0600

    added onclick to capture user's burger into var userBurger

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,12 @@
-const burger = require("./orm.js");
+// const burger = require("./orm.js");
 
+var userBurger;
 
+$("#submit").on("click", function() {
+  userBurger = $("#burger-input").val().trim();
+  console.log(userBurger);
+
+});
 
 
 //create the code that will call the ORM functions using burger specific input for the ORM
