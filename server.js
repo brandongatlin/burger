3e89ea018bbea83a340492a6f2262971006825a5
brandongatlin <brandongatlin1981@me.com>
2017-12-22 16:09:40 -0600

    added connection to db

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
+// const connection = require("connection");
 
 const app = express();
+
+const port = 3000;
+
+app.listen(port);
+
+console.log("listening on port:", port);
